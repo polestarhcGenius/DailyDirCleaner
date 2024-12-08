b7ccd818dc59e849819401b7372ba39888556afa
kyj1996 <kyj1435@polestarhc.com>
2024-12-08 21:53:07 +0900

    [feat] 삭제 폴더 지정 모달에서 추가 및 삭제가 즉시 반영되도록 개선

--- src/main/java/org/example/MainFrame.java
+++ src/main/java/org/example/MainFrame.java
@@ -4,11 +4,20 @@ import javax.swing.*;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.util.List;
 
 public class MainFrame extends JFrame implements ActionListener {
     Container cp;
     JButton btnImmediatelyDelete, btnScheduleDelete, btnSetDir;
+    FileUtil fileUtil = new FileUtil();
+    TimeUtil timeUtil;
+    JTextField textField;
 
+    JTextArea resultTextArea;
+
+    JPanel cmdPanel;
+
+    JLabel remainTime;
 
 
     public MainFrame(String title) {
@@ -21,8 +30,8 @@ public class MainFrame extends JFrame implements ActionListener {
     }
 
     public void setDesign(){
-        this.setLayout(new FlowLayout(FlowLayout.RIGHT));
-        // this.setLayout(new FlowLayout());
+        this.setLayout(new FlowLayout(FlowLayout.LEFT));
+        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
 
         // 160 40
@@ -37,12 +46,6 @@ public class MainFrame extends JFrame implements ActionListener {
         btnImmediatelyDelete.setFont(new Font("default", Font.BOLD, 16));
         btnScheduleDelete.setFont(new Font("default", Font.BOLD, 16));
         btnSetDir.setFont(new Font("default", Font.BOLD, 16));
-
-
-
-        this.add(btnImmediatelyDelete);
-        this.add(btnScheduleDelete);
-        this.add(btnSetDir);
 
         btnImmediatelyDelete.setBackground(new Color(228, 228, 228));
         btnImmediatelyDelete.setOpaque(true);
@@ -60,12 +63,59 @@ public class MainFrame extends JFrame implements ActionListener {
         btnScheduleDelete.addActionListener(this);
         btnSetDir.addActionListener(this);
 
-//        System.out.println(btnImmediatelyDelete.getPreferredSize());
-//        System.out.println(btnScheduleDelete.getPreferredSize());
-//        System.out.println(btnSetDir.getPreferredSize());
+        JLabel jLabel = new JLabel("파일 삭제 프로그램");
+        ImageIcon icon = new ImageIcon("src/main/resources/img/pshc_logo.png");
+        jLabel.setIcon(icon);
+        this.setIconImage(icon.getImage());
+
+        jLabel.setFont(new Font("default", Font.BOLD, 22));
+        this.add(jLabel);
+
+        this.add(Box.createRigidArea(new Dimension(70, 0)));
+
+        this.add(btnImmediatelyDelete);
+        this.add(btnScheduleDelete);
+        this.add(btnSetDir);
+
+
+        this.add(new JLabel("다음 삭제까지 남은 시간"));
+
+
+        remainTime = new JLabel("00:00:00");
+        this.add(remainTime);
+
+
+        this.add(Box.createRigidArea(new Dimension(350, 0)));
+
+
+        this.add(new JLabel("예약시간 매일"));
+
+        textField = new JTextField(3);
+
+        this.add(textField);
+
+        this.add(new JLabel("시 삭제(입력값 : 0~23)"));
+
+
+//        cmdPanel = new JPanel();
+//        cmdPanel.setBackground(new Color(228, 228, 228));
+//        cmdPanel.setLayout(new FlowLayout(FlowLayout.LEFT));
+//        cmdPanel.setPreferredSize(new Dimension(160,40));
+
+        resultTextArea = new JTextArea();
+        resultTextArea.setPreferredSize(new Dimension(800,350));
+        resultTextArea.setLineWrap(true); // 자동 줄바꿈
+        resultTextArea.setWrapStyleWord(true); // 들여 쓰기
+        resultTextArea.setEditable(false);
+        JScrollPane scrollPane = new JScrollPane(resultTextArea);
+//        cmdPanel.add(resultTextArea);
+        this.add(scrollPane);
+
 
 
     }
+
+
 
     @Override
     public void actionPerformed(ActionEvent e){
@@ -73,19 +123,35 @@ public class MainFrame extends JFrame implements ActionListener {
 
         if(source==btnImmediatelyDelete){
             JOptionPane.showMessageDialog(this, "즉시삭제 버튼클릭");
+            List<String> ymlData =  fileUtil.yamlDataLoader();
+            fileUtil.deleteFile(ymlData);
         } else if (source==btnScheduleDelete){
-            JOptionPane.showMessageDialog(this, "예약삭제 버튼클릭");
             if (btnScheduleDelete.getText() == "예약삭제"){
+                if (textField.getText().isEmpty()){
+                    JOptionPane.showMessageDialog(this, "예약 시간을 지정해 주세요.");
+                    return;
+                }
+
+
+                timeUtil = new TimeUtil(textField.getText(), remainTime);
+                timeUtil.start();
+
+
+
+                textField.setEditable(false);
                 btnScheduleDelete.setText("예약삭제중단");
                 btnSetDir.setEnabled(false);
             } else{
+                timeUtil.interrupt();
                 btnScheduleDelete.setText("예약삭제");
                 btnSetDir.setEnabled(true);
+                textField.setEditable(true);
             }
-
         } else if (source==btnSetDir) {
-            JOptionPane.showMessageDialog(this, "3번째 버튼클릭");
+            new SetDirDialog(this, "삭제 폴더 지정", true);
         }
+
+
     }
 
 
