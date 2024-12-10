cddc5b3c6da6e43e14a804903a82881bdc4b0b60
kyj1996 <kyj1435@polestarhc.com>
2024-12-10 10:11:29 +0900

    [feat] 드라이브 잔량 기능 추가

--- src/main/java/org/example/FileUtil.java
+++ src/main/java/org/example/FileUtil.java
@@ -6,37 +6,39 @@ import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.nio.file.attribute.FileTime;
+import java.time.Clock;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 import java.util.*;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 public class FileUtil {
+    private static final Logger logger = LoggerFactory.getLogger(FileUtil.class);
+
     //삭제 대상 폴더 정보가 담긴 yml 파일 읽기
     public List<String> yamlDataLoader(){
         try{
             Map<String, List<String>> dirData = new Yaml().load(new FileReader("DirConfig.yml"));
             return dirData.get("dir");
         } catch (Exception e) {
-            JOptionPane.showMessageDialog(null, "설정파일 DirConfig.yml이 존재하지 않습니다. 프로그램을 종료합니다.");
-            e.printStackTrace();
-            throw new RuntimeException(e);
+            logger.error(e.getMessage());
+            try{
+                Map<String, List<String>> dirData = new Yaml().load(new FileReader("src/main/resources/DirConfig.yml"));
+                return dirData.get("dir");
+            } catch (FileNotFoundException ex) {
+                throw new RuntimeException(ex);
+            }
         }
     }
 
     public void yamlDataWriter(List<String> dirs) throws FileNotFoundException {
-
-//        String[] dirList = dirs.split("\n");
-//        List<String> result = new ArrayList<>();
-//        for (String dir : dirList) {
-//            result.add(dir);
-//        }
-
         Map<String, List<String>> data = new LinkedHashMap<String, List<String>>();
         data.put("dir", dirs);
 
         Yaml yaml = new Yaml();
         PrintWriter writer = new PrintWriter(new File("DirConfig.yml"));
         yaml.dump(data, writer);
-
-
     }
 
     // 대상 파일의 생성 시간 조회
@@ -73,40 +75,65 @@ public class FileUtil {
         return result;
     }
 
+    public String getDiskInfo(){
+        String result = "";
+        String drive;
+        double totalSize, freeSize, useSize;
+        File[] roots = File.listRoots();
+        for (File root : roots) {
+            drive = root.getAbsolutePath();
+            totalSize = root.getTotalSpace() / (1024.0*1024*1024);
+            freeSize = root.getFreeSpace() / (1024.0*1024*1024);
+            result += drive + " 드라이브 남은 용량 : " + Math.floor(freeSize/totalSize*100) + "%\n";
+        }
+        return result;
+    }
 
     public void deleteFile(List<String> pathList, JTextArea resultTextArea){
+        LocalDateTime localDateTime = LocalDateTime.now(Clock.systemDefaultZone());
+        String startTime = localDateTime.format(DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss"));
+        logger.info("\n< {} 삭제 시작>\n", startTime);
+        resultTextArea.append("\n < " + startTime + " 삭제 시작>\n");
+        
         String logData = "";
         for (String path : pathList){
-            if (!new File(path).isDirectory()){ // 경로 유효성 검증
-                // JOptionPane.showMessageDialog(null, "디렉토리" + path + "는 존재하지 않거나, 올바르지 않은 경로입니다.");
-                logData += path + ": 해당 디렉토리는 존재하지 않거나, 올바르지 않은 경로입니다.\n";
-                continue;
-            }
+
             List<String> fileList = getFileList(path);
             int totalCount = fileList.size();
             int errorCount = 0;
             String err_msg = "";
 
+            if (!new File(path).isDirectory()){ // 경로 유효성 검증
+                logData += path + ": 해당 디렉토리는 존재하지 않거나, 올바르지 않은 경로입니다.\n";
+                continue;
+            }
+
             for (String file : fileList){
                 Path filePath = Paths.get(file);
                 try{
                     Files.delete(filePath);
-                    System.out.println("Deleted file: " + file);
+                    logger.info("Deleted file: {}", file);
                     resultTextArea.append("Deleted file: " + file + "\n");
                 } catch (IOException e) {
                     errorCount++;
-                    System.out.println(e);
                     err_msg += e + "\n";
+                    logger.error("Failed to delete file: " + file + " " + e + "\n");
                     resultTextArea.append("Failed to delete file: " + file + " " + e + "\n");
                 }
             }
 
             int successCount = totalCount - errorCount;
-            logData += path + "총 " + totalCount + "건 중" + successCount + "건 성공, " + errorCount + "건 실패\n";
+            logData += path + " 총 " + totalCount + "건 중 " + successCount + "건 성공, " + errorCount + "건 실패\n";
             logData += err_msg + "\n";
         }
-        resultTextArea.append("\n<삭제 결과>\n");
+
+        localDateTime = LocalDateTime.now(Clock.systemDefaultZone());
+        String endTime = localDateTime.format(DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss"));
+        String diskInfo = getDiskInfo();
+        logger.info("\n< {} 삭제 결과>\n {} \n {}", endTime, logData, diskInfo);
+        resultTextArea.append("\n < " + endTime + " 삭제 결과>\n");
         resultTextArea.append(logData);
+        resultTextArea.append(diskInfo);
     }
 
 }
